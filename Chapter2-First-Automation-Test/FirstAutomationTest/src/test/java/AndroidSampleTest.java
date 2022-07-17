7f030e68cdf84f4dd20c2f2cda5a4e35162a5fef
Alexander Petrovskiy <apetrovskiy2@hotmail.com>
2022-07-17 17:27:39 +0300

    Update AndroidSampleTest.java

--- Chapter2-First-Automation-Test/FirstAutomationTest/src/test/java/AndroidSampleTest.java
+++ Chapter2-First-Automation-Test/FirstAutomationTest/src/test/java/AndroidSampleTest.java
@@ -23,7 +23,8 @@ public class AndroidSampleTest {
         DesiredCapabilities dc = new DesiredCapabilities();
         dc.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
         dc.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.0");
-        dc.setCapability(MobileCapabilityType.APP, "/Users/alexanderpetrovskiy/projects/Java/forks/appiumbook/Chapter2-First-Automation-Test/FirstAutomationTest/src/test/resources/AndroidCalculator.apk");
+        System.out.println(AndroidSampleTest.class.getClassLoader().getResource("AndroidCalculator.apk").getPath());
+        dc.setCapability(MobileCapabilityType.APP, AndroidSampleTest.class.getClassLoader().getResource("AndroidCalculator.apk").getPath());
         dc.setCapability(MobileCapabilityType.DEVICE_NAME, "emulator-5554");
         dc.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
 
